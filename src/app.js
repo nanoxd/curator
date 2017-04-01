34804a40824d45e3b3f12746bd78012267f4e9a1
Fernando Paredes <nano@fdp.io>
2017-03-31 21:58:13 -0700

    Use JSON for body parsing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 
+// Middleware
+app.use(bodyParser.json())
+
+// Routes
 app.get('/', (req, res) => {
   res.json({ message: '👋' })
 })
