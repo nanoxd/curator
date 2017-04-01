bc9541ce5f9a7d794e59495ee97f5c840be88aea
Fernando Paredes <nano@fdp.io>
2017-03-31 21:22:13 -0700

    Setup basic server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+import express from 'express'
+
+const { PORT = 3000 } = process.env
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ message: '👋' })
+})
+
+app.listen(
+  PORT,
+  () => console.log(`Listening on port ${PORT}`)
+)
